7080a288b0574925208acd72988362e500ee9cce
thiagohernandez <thiago@laclave.es>
2021-06-11 10:09:46 +0200

    Update frontend.js

--- wp-content/themes/psyke/js/frontend.js
+++ wp-content/themes/psyke/js/frontend.js
@@ -551,7 +551,36 @@
         });
         
         
-       
+        
+    /*
+    // LOADING AJAX
+    //
+    // List of Contact Form 7 Custom DOM Events
+    // wpcf7invalid — Fires when an Ajax form submission has completed successfully, but mail hasn’t been sent because there are fields with invalid input.
+    // wpcf7spam — Fires when an Ajax form submission has completed successfully, but mail hasn’t been sent because a possible spam activity has been detected.
+    // wpcf7mailsent — Fires when an Ajax form submission has completed successfully, and mail has been sent.
+    // wpcf7mailfailed — Fires when an Ajax form submission has completed successfully, but it has failed in sending mail.
+    // wpcf7submit — Fires when an Ajax form submission has completed successfully, regardless of other incidents.
+    //
+    */  
+
+
+      /*
+      // REDIRECT AND LOADING EFFECTS
+      */
+
+      var baseURL = window.location.hostname + '/develop/psyke';
+      //var baseURL = window.location.hostname;
+
+      //form user
+      if($('#wpcf7-f443-p445-o1').length > 0) {
+        document.addEventListener( 'wpcf7mailsent', function( event ) {
+          console.log('trigger form redirect');
+            location = baseURL + '/gracias-por-ponerte-en-contacto/';
+        }, false );
+      }
+      
+
         
 
           
